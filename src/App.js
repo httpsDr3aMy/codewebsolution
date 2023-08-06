4dd2719ce4a3998aa757e28b6f82f3df3f4df631
httpsDr3aMy <xkamilptx@gmail.com>
2023-08-06 20:02:53 +0200

    main test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Aboutme from './components/Aboutme';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
